3062067518502d78bb7e8aef7d50d1cb66eaf1eb
dcazabat <dacazabat@gmail.com>
2021-07-08 00:04:34 -0300

    Select

--- src/app.py
+++ src/app.py
@@ -6,16 +6,16 @@ app = Flask(__name__)
 mysql = MySQL()
 
 # En remote host
-# app.config['MYSQL_DATABASE_HOST'] = 'remotemysql.com'
-# app.config['MYSQL_DATABASE_USER'] = 'rB3cUolUmD'
-# app.config['MYSQL_DATABASE_PASSWORD'] = 'vXlnbLUILD'
-# app.config['MYSQL_DATABASE_DB'] = 'rB3cUolUmD'
+app.config['MYSQL_DATABASE_HOST'] = 'remotemysql.com'
+app.config['MYSQL_DATABASE_USER'] = 'rB3cUolUmD'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'vXlnbLUILD'
+app.config['MYSQL_DATABASE_DB'] = 'rB3cUolUmD'
 
 # En localhost
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'empleados'
+# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+# app.config['MYSQL_DATABASE_USER'] = 'root'
+# app.config['MYSQL_DATABASE_PASSWORD'] = ''
+# app.config['MYSQL_DATABASE_DB'] = 'empleados'
 
 
 mysql.init_app(app)
@@ -23,7 +23,7 @@ mysql.init_app(app)
 @app.route('/')
 def index():
     #sql = 'insert into empleados (nombre, correo, foto) values ("Daniela Leon", "mdel2002@hotmail.com", "fotodedaniela.png");'
-    sql = 'SELECT * FROM `empleados`.`empleados`'
+    sql = 'SELECT * FROM empleados'
     conn = mysql.connect()
     cursor = conn.cursor()
     cursor.execute(sql)
