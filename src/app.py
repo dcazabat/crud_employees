9e19d5ba7918a980183a68b154db82bcd1b7e2c7
dcazabat <dacazabat@gmail.com>
2021-07-19 22:09:51 -0300

    Todo OK
    Empleados y Usuarios OK

--- src/app.py
+++ src/app.py
@@ -1,12 +1,14 @@
+from os import abort
 import pymysql
-from flask import Flask, flash, render_template, request, redirect, session
+from flask import Flask, flash, render_template, request, redirect, session, send_from_directory
 # from cryptography.fernet import Fernet
 from flaskext.mysql import MySQL
-
+from datetime import datetime
+import os
 
 app = Flask(__name__)
-# key = "daniel"
-# encrypted_key = Fernet(key)
+app.secret_key = "daniel"
+# encrypted_key = Fernet(app.secret_key)
 mysql = MySQL()
 
 # MySQL configurations
@@ -28,11 +30,9 @@ mysql.init_app(app)
 # Diccionario de que Contiene parametros Globales de la App
 data={
     'title':'App de Flask - Empleados',
-    'message':'Bienvenidos al Sitio',
+    'message': '',
     'messagealternative':'',
-    'errbd': False,
-    'session': '',
-    'username': ''
+    'errbd': False
 }
 
 def test_connection():
@@ -56,6 +56,7 @@ def page_not_found(error):
 @app.route('/home')
 def index():
     test_connection()
+    data['message'] = 'Bienvenidos al Sitio'
     if data['errbd']:
         data['messagealternative'] = 'Sin Conexión a la Base de Datos'
     else:
@@ -76,8 +77,9 @@ def login():
             cursor.execute(sql)
             cursor = cursor.fetchall()
             if cursor[0]['user_password'] == _pass:
-                data['session'] = 'OK'
-                data['username'] = cursor[0]['user_name']
+                session['username'] = cursor[0]['user_name']
+                session['email'] = cursor[0]['user_email']
+                session['admin'] = cursor[0]['user_isadmin'] == 1
         return redirect('/')
     except pymysql.err.OperationalError:
         data['errbd'] = True
@@ -88,9 +90,12 @@ def login():
 
 @app.route('/logout')
 def logout():
-    data['session'] = ''
-    data['username'] = ''
+    session.pop('username', None)
+    session.pop('email', None)
+    session.pop('admin', None)
     return redirect('/')
+
+# TODO Emplaados
 
 @app.route('/employees')
 def employees():
@@ -104,8 +109,7 @@ def employees():
         cursor = conn.cursor(pymysql.cursors.DictCursor)
         cursor.execute(sql)
         cursor = cursor.fetchall()
-        cabecera = list(cursor[0].keys())
-        cabecera.remove('id')
+        cabecera = ['#', 'Foto', 'Nombre', 'Correo', 'Acciones']
         data['message'] = 'Lista de Empleados'
 
         return render_template('empleados/employees.html', cursor=cursor, cabecera=cabecera, data=data)
@@ -115,21 +119,151 @@ def employees():
     except Exception as e:
         return e
 
+@app.route('/fotodeusuario/<path:nombreFoto>')
+def uploads(nombreFoto):
+    print('**************',send_from_directory(os.path.join('uploads'), nombreFoto))
+    return send_from_directory(os.path.join('uploads'), nombreFoto)
 
-@app.route('/users')
+@app.route('/newemployee')
+def add_employee_view():
+    data['message'] = 'Alta de Empleados'
+    return render_template('empleados/add.html',data=data)
+
+@app.route('/addemployee',  methods=['GET', 'POST'])
+def add_employee():
+    cursor = None
+    try: 
+        if session['username']:
+            _name = request.form['inputName']
+            _email = request.form['inputEmail']
+            _foto = request.files['inputFoto']
+            # validate the received values
+            if _name and _email and _foto and request.method == 'POST':
+                now = datetime.now()
+                tiempo = now.strftime("%Y%H%M%S")
+                if _foto.filename != '':
+                    nuevoNombreFoto = tiempo + '_' + _foto.filename
+                    _foto.save("src/static/assets/images/uploads/" + nuevoNombreFoto)
+
+                sql = "INSERT INTO empleados (nombre, correo, foto) values (%s, %s, %s);"
+                datos = (_name, _email, nuevoNombreFoto)                # save edits
+                conn = mysql.connect()
+                cursor = conn.cursor()
+                cursor.execute(sql, datos)
+                conn.commit()
+            return redirect('/employees')
+        else:
+            return render_template('404.html'), 404
+    except Exception as e:
+        return e
+
+
+@app.route('/editemployee/<int:id>',  methods=['GET', 'POST'])
+def edit_employee(id):
+    cursor = None
+    data['message'] = 'Editar de Empleado'
+    try:
+        if session['username']:
+            conn = mysql.connect()
+            cursor = conn.cursor(pymysql.cursors.DictCursor)
+            datos = [id]
+            cursor.execute("SELECT * FROM empleados WHERE id=%s", datos)
+            row = cursor.fetchone()
+            return render_template('empleados/edit.html', row=row, data=data)
+        else:
+            return render_template('404.html'), 404
+    except Exception as e:
+        return e
+
+
+@app.route('/updateemployee', methods=['GET', 'POST'])
+def update_employee():
+    cursor = None
+    try:
+        if session['username']:
+            _name = request.form['inputName']
+            _email = request.form['inputEmail']
+            _foto = request.files['inputFoto']
+            _id = request.form['inputId']
+            # validate the received values
+            if _name and _email and _id and request.method == 'POST':
+                # save edits
+                conn = mysql.connect()
+                cursor = conn.cursor(pymysql.cursors.DictCursor)
+                if _foto.filename != '':
+                    now = datetime.now()
+                    tiempo = now.strftime("%Y%H%M%S")
+                    nuevoNombreFoto = tiempo + '_' + _foto.filename
+                    print(nuevoNombreFoto)
+                    _foto.save("static/assets/images/uploads/" + nuevoNombreFoto)
+
+                    sql = f'SELECT foto FROM empleados WHERE id={_id}'
+                    cursor.execute(sql)
+                    row = cursor.fetchone()
+                    try:
+                        os.remove(os.path.join(app.config['UPLOADS'], row['foto']))
+                    except:
+                        pass
+
+                    sql = f'UPDATE empleados SET foto="{nuevoNombreFoto}" WHERE id={_id};'
+                    cursor.execute(sql)
+                    conn.commit()
+
+                sql = f'UPDATE empleados SET nombre="{_name}", correo="{_email}" WHERE id={_id}'
+                cursor.execute(sql)
+                conn.commit()
+                return redirect('/employees')
+        else:
+            return render_template('404.html'), 404
+    except Exception as e:
+        return e
+
+@app.route('/deleteemployee/<int:id>')
+def delete_employee(id):
+    cursor = None
+    try:
+        if session['username']:
+            conn = mysql.connect()
+            cursor = conn.cursor(pymysql.cursors.DictCursor)
+            sql = "SELECT foto FROM empleados WHERE id = (%s)"
+            datos = [id]
+            cursor.execute(sql, datos)
+            row = cursor.fetchone()
+
+            try:
+                os.remove(os.path.join(app.config['UPLOADS'], row['foto']))
+            except:
+                pass
+
+            sql = "DELETE FROM empleados WHERE id = (%s)"
+            cursor.execute(sql, datos)
+            conn.commit()
+            return redirect('/employees')
+        else:
+            return render_template('404.html'), 404
+    except Exception as e:
+        return e
+
+
+# TODO Users
+
+@app.route('/users', methods=['GET', 'POST'])   
 def users():
     cursor = None    
     try:
-        sql = 'SELECT * FROM tbl_user'
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute(sql)
-        cursor = cursor.fetchall()
-        print(cursor)
-        cabecera = ['Nombre', 'E-mail', 'Acciones']
-        data['message'] = 'Usuarios del Sistema'
+        if session['admin']:
+            sql = 'SELECT * FROM tbl_user'
+            conn = mysql.connect()
+            cursor = conn.cursor(pymysql.cursors.DictCursor)
+            cursor.execute(sql)
+            cursor = cursor.fetchall()
+            print(cursor)
+            cabecera = ['Nombre', 'E-mail', 'Acciones']
+            data['message'] = 'Usuarios del Sistema'
 
-        return render_template('login/users.html', cursor=cursor, cabecera=cabecera, data=data)
+            return render_template('login/users.html', cursor=cursor, cabecera=cabecera, data=data)
+        else:
+           return render_template('404.html'), 404
     except pymysql.err.OperationalError:
         data['errbd'] = True
         return render_template('login/users.html', data=data)
@@ -137,81 +271,93 @@ def users():
         return e
 
 
-@app.route('/new_user')
+@app.route('/newuser')
 def add_user_view():
     data['message'] = 'Alta de Usuarios'
     return render_template('login/add.html',data=data)
 
-@app.route('/add',  methods=['GET', 'POST'])
+@app.route('/adduser',  methods=['GET', 'POST'])
 def add_user():
     cursor = None
     try: 
-        _name = request.form['inputName']
-        _email = request.form['inputEmail']
-        _password = request.form['inputPassword']
-        # validate the received values
-        if _name and _email and _password and request.method == 'POST':
-        #do not save password as a plain text
-            _hashed_password = _password
-            # _hashed_password = encrypted_key.encrypt(_password)
-            # save edits
-            sql = "INSERT INTO tbl_user(user_name, user_email, user_password) VALUES(%s, %s, %s)"
-            datos = (_name, _email, _hashed_password)
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            cursor.execute(sql, datos)
-            conn.commit()
-        return redirect('/users')
+        if session['admin']:
+            _name = request.form['inputName']
+            _email = request.form['inputEmail']
+            _password = request.form['inputPassword']
+            # validate the received values
+            if _name and _email and _password and request.method == 'POST':
+            #do not save password as a plain text
+                _hashed_password = _password
+                # _hashed_password = encrypted_key.encrypt(_password)
+                # save edits
+                sql = "INSERT INTO tbl_user(user_name, user_email, user_password) VALUES(%s, %s, %s)"
+                datos = (_name, _email, _hashed_password)
+                conn = mysql.connect()
+                cursor = conn.cursor()
+                cursor.execute(sql, datos)
+                conn.commit()
+            return redirect('/users')
+        else:
+            return render_template('404.html'), 404
     except Exception as e:
         return e
 
 
-@app.route('/edit/<int:id>')
+@app.route('/edituser/<int:id>')
 def edit_user(id):
     cursor = None
     data['message'] = 'Editar de Usuario'
     try:
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT * FROM tbl_user WHERE user_id=%s", id)
-        row = cursor.fetchone()
-        return render_template('login/edit.html', row=row, data=data)
+        if session['admin']:
+            conn = mysql.connect()
+            cursor = conn.cursor(pymysql.cursors.DictCursor)
+            cursor.execute("SELECT * FROM tbl_user WHERE user_id=%s", id)
+            row = cursor.fetchone()
+            return render_template('login/edit.html', row=row, data=data)
+        else:
+            return render_template('404.html'), 404
     except Exception as e:
         return e
 
 
-@app.route('/update', methods=['GET', 'POST'])
+@app.route('/updateuser', methods=['GET', 'POST'])
 def update_user():
     cursor = None
-    try: 
-        _name = request.form['inputName']
-        _password = request.form['inputPassword']
-        _id = request.form['inputId']
-        # validate the received values
-        if _name and _password and _id and request.method == 'POST':
-            #do not save password as a plain text
-            # _hashed_password = encrypted_key.encrypt(_password)
-            _hashed_password = _password
-            # save edits
-            sql = "UPDATE tbl_user SET user_name=%s, user_password=%s WHERE user_id=%s"
-            datos = (_name, _hashed_password, _id,)
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            cursor.execute(sql, datos)
-            conn.commit()
-            return redirect('/users')
+    try:
+        if session['admin']:
+            _name = request.form['inputName']
+            _password = request.form['inputPassword']
+            _id = request.form['inputId']
+            # validate the received values
+            if _name and _password and _id and request.method == 'POST':
+                #do not save password as a plain text
+                # _hashed_password = encrypted_key.encrypt(_password)
+                _hashed_password = _password
+                # save edits
+                sql = "UPDATE tbl_user SET user_name=%s, user_password=%s WHERE user_id=%s"
+                datos = (_name, _hashed_password, _id,)
+                conn = mysql.connect()
+                cursor = conn.cursor()
+                cursor.execute(sql, datos)
+                conn.commit()
+                return redirect('/users')
+        else:
+            return render_template('404.html'), 404
     except Exception as e:
         return e
 
-@app.route('/delete/<int:id>')
+@app.route('/deleteuser/<int:id>')
 def delete_user(id):
     cursor = None
     try:
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        cursor.execute("DELETE FROM tbl_user WHERE user_id=%s", (id,))
-        conn.commit()
-        return redirect('/users')
+        if session['admin']:
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.execute("DELETE FROM tbl_user WHERE user_id=%s", (id,))
+            conn.commit()
+            return redirect('/users')
+        else:
+            return render_template('404.html'), 404
     except Exception as e:
         return e
 
